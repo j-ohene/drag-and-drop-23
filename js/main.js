d9d32f8d6bf7b69bd11d20f1fb644086af8bc529
j-ohene <jesicaohene1@gmail.com>
2023-06-06 16:51:05 -0400

    added event listeners and drag starts

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //variables
 const theButtons = document.querySelectorAll("#buttonHolder img"),
-puzzleBoard = document.querySelector(".puzzle-board");
+puzzleBoard = document.querySelector(".puzzle-board"),
+puzzlePieces = document.querySelectorAll(".puzzle-pieces img")
 //console.log(theButtons);
 //console.log(puzzleBoard);
 
@@ -9,7 +10,12 @@ function changeBGImage() {
     //url('../images/backGround0.jpg);
     puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`
 }
+function handleStartDrag(){
+    console.log("Started dragging this piece:", this)
+}
+
 
 //event Listeners
 theButtons.forEach(button => button.addEventListener("click", changeBGImage));
 
+puzzlePieces.forEach(piece=> piece.addEventListener("dragstart", handleStartDrag));
